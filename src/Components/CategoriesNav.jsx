f153a915bfbc5b6089120898612896fc8064facf
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-07-23 16:15:17 +0530

    Categories Navbar added

--- src/Components/CategoriesNav.jsx
+++ src/Components/CategoriesNav.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function CategoriesNav() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <ul className="categories_nav">
+        <li>
+          <a href="#">All</a>
+        </li>
+        <li>
+          <a href="#">Amazon Pay</a>
+        </li>
+        <li>
+          <a href="#">Mobiles</a>
+        </li>
+        <li>
+          <a href="#">Fashion</a>
+        </li>
+        <li>
+          <a href="#">Electronics</a>
+        </li>
+        <li>
+          <a href="#">New Releases</a>
+        </li>
+        <li>
+          <a href="#">Prime</a>
+        </li>
+        <li>
+          <a href="#">Computers</a>
+        </li>
+        <li>
+          <a href="#">Pet Supplies</a>
+        </li>
+        <li>
+          <a href="#">Health, Household & Personal Care</a>
+        </li>
+        <li>
+          <a href="#">Gift Ideas</a>
+        </li>
+        <li>
+          <a href="#">Customer Service</a>
+        </li>
+      </ul>
+    </>
+  );
 }
 
-export default CategoriesNav
+export default CategoriesNav;
