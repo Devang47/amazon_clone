97da3277f219035e5365aef4a7f1819e5c961c67
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-07-23 11:20:33 +0530

    + Location set box done

--- src/Components/LocationBox.jsx
+++ src/Components/LocationBox.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 
-function LocationBox() {
+function LocationBox(props) {
+
+
+  const getLocation = () => {
+    
+  }
+
   return (
     <>
-      <section className="set_location_section">
-        <button className="shadow"></button>
+      <section
+        className="set_location_section"
+       
+      >
+        <div className="shadow" onClick={() => props.setLBox(false)}></div>
         <div className="box">
           <div className="top_bar bold">Choose your location</div>
           <div className="inner_content">
             <p className="select_a_delivery">
-              Select a delivery location to see product availability and delivery options
+              Select a delivery location to see product availability and
+              delivery options
             </p>
             <button className="sign_in">Sign in to see your addresses</button>
             <span className="or_enter_pincode">or enter an Indian pincode</span>
-            <form className="pincode_input">
+            <form className="pincode_input" onSubmit={getLocation}>
               <input type="text" />
-              <button className="apply" type='submit'>Apply</button>
+              <button className="apply" type="submit">
+                Apply
+              </button>
             </form>
           </div>
         </div>
