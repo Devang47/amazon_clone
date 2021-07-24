24338c55b417ef7286615d2d4d38f7bcde395cfc
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-07-24 12:25:46 +0530

    Prettier config added and page formatted

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useContext } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 
-import Navbar from "./Components/Navbar/Navbar";
-import LocationBox from "./Components/Navbar/LocationBox";
-import "./Styles/style.css";
-import Homepage_Products from "./Components/Homepage/Homepage-products";
+import Navbar from './Components/Navbar/Navbar';
+import LocationBox from './Components/Navbar/LocationBox';
+import './Styles/style.css';
+import Homepage_Products from './Components/Homepage/HomepageProducts';
 
 function App() {
-  const routes = ["/register", "/signin"];
-  const location = useLocation();
-  const [locationBox, setLocationBox] = useState(false);
-  const [user_location, setUserLocation] = useState("Select your address");
-  return (
-    <>
-      {!routes.includes(location.pathname) && (
-        <Navbar location={user_location} setLBox={setLocationBox} />
-      )}
-      {locationBox && (
-        <LocationBox setLBox={setLocationBox} setLocation={setUserLocation} />
-      )}
-      <Homepage_Products />
-    </>
-  );
+	const routes = ['/register', '/signin'];
+	const location = useLocation();
+	const [locationBox, setLocationBox] = useState(false);
+	const [user_location, setUserLocation] = useState('Select your address');
+	return (
+		<>
+			{!routes.includes(location.pathname) && (
+				<Navbar location={user_location} setLBox={setLocationBox} />
+			)}
+			{locationBox && (
+				<LocationBox setLBox={setLocationBox} setLocation={setUserLocation} />
+			)}
+			<Homepage_Products />
+		</>
+	);
 }
 
 export default App;
