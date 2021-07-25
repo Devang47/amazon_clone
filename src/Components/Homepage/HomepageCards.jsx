fb8ab95472a1895756cff34bd09e15981a78a37b
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-07-25 08:16:27 +0530

    Homepage cards added

---
+++ src/Components/Homepage/HomepageCards.jsx
@@ -0,0 +1,428 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomepageCards(Props) {
+  return (
+    <>
+      <div className="item" id="item1">
+        <h2>Upgrade your home | Amazon Brands & more</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+
+      <div className="item" id="item2">
+        <h2>Top rated, premium quality | Amazon Brands & more</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item3">
+        <h2>Exclusively for Prime members</h2>
+        <div className="banner_img-wrapper">
+          <span className="banner_img" src="#" alt="Display Image"></span>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item4">
+        <h2 className="sign_in_title">Sign in for your best experience</h2>
+        <button className="sign_in_btn">Sign in securely</button>
+      </div>
+      <div className="item" id="item5">
+        <h2>Styles for Men | Up to 70% off</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item6">
+        <h2>Automotive essentials | Up to 60% off</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item7">
+        <h2>Shop by Category</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item8">
+        <h2 className="lightH2">Top picks for your home ACs</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item9">
+        <h2 className="lightH2">Today’s Top Deals</h2>
+        <div className="homepage_item_slider">
+          {[1, 2, 3, 4, 5].map(e => {
+            return (
+              <>
+                <Link to="#">
+                  <div className="slider_item">
+                    <div className="item_thumbnail">
+                      <img
+                        src="https://images-na.ssl-images-amazon.com/images/I/91qtcZ6c2yL._SX679_.jpg"
+                        alt="Item thumbnail"
+                      />
+                    </div>
+                    <div className="item_details">
+                      <span className="price_range">
+                        <span className="currency_sign">$</span>179.00 -
+                        <span className="currency_sign">$</span>20,99.00
+                      </span>
+                      <br />
+                      <span className="timer">Ends in 02:14:34</span>
+                    </div>
+                  </div>
+                </Link>
+                <Link to="#">
+                  <div className="slider_item">
+                    <div className="item_thumbnail">
+                      <img
+                        src="https://images-eu.ssl-images-amazon.com/images/I/41znff7SgPL._SX300_SY300_QL70_FMwebp_.jpg"
+                        alt="Item thumbnail"
+                      />
+                    </div>
+                    <div className="item_details">
+                      <span className="price">
+                        <span className="currency_sign">$</span>20,99.00
+                      </span>
+                      <br />
+
+                      <span className="timer">Ends in 02:14:34</span>
+                    </div>
+                  </div>
+                </Link>
+              </>
+            );
+          })}
+        </div>
+      </div>
+      <div className="item" id="item10">
+        <h2>Effective dishwashing for Indian cooking</h2>
+        <div className="banner_img-wrapper">
+          <span className="banner_img" src="#" alt="Display Image"></span>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item11">
+        <h2>Up to 45% off | Bedding & linen | Amazon Brands & more</h2>
+        <div className="banner_img-wrapper">
+          <span className="banner_img" src="#" alt="Display Image"></span>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item12">
+        <h2>Flat 50% back on Fruits & vegetables | Amazon Fresh</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+      <div className="item" id="item12">
+        <h2>At home Grooming & Wellness devices</h2>
+        <div className="inner_card">
+          <div className="inr_itm inr_itm1">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm2">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm3">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+
+          <div className="inr_itm inr_itm4">
+            <a href="#">
+              <span className="img"></span>
+              <span className="caption">Smart LED TVs</span>
+            </a>
+          </div>
+        </div>
+        <div className="read_more">
+          <a href="#">See more</a>
+        </div>
+      </div>
+
+      <div className="item" id="item13">
+        <h2 className="lightH2">
+          Curated protective supply products | Amazon Launchpad
+        </h2>
+        <div className="homepage_item_slider">
+          {[1, 2, 3, 4, 5].map(e => {
+            return (
+              <>
+                <a href="#">
+                  <div className="slider_item">
+                    <img
+                      src="https://images-eu.ssl-images-amazon.com/images/I/41znff7SgPL._SX300_SY300_QL70_FMwebp_.jpg"
+                      alt="Image"
+                    />
+                  </div>
+                </a>
+                <a href="#">
+                  <div className="slider_item">
+                    <img
+                      src="https://images-na.ssl-images-amazon.com/images/I/91qtcZ6c2yL._SX679_.jpg"
+                      alt="Image"
+                    />
+                  </div>
+                </a>
+              </>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="item" id="item14">
+        <h2 className="lightH2">Explore everyday essentials</h2>
+        <div className="homepage_item_slider">
+          {[1, 2, 3, 4, 5].map(e => {
+            return (
+              <>
+                <a href="#">
+                  <div className="slider_item">
+                    <img
+                      src="https://images-eu.ssl-images-amazon.com/images/I/51ceWNX9JzL._SX300_SY300_QL70_FMwebp_.jpg"
+                      alt="Image"
+                    />
+                  </div>
+                </a>
+                <a href="#">
+                  <div className="slider_item">
+                    <img
+                      src="https://images-eu.ssl-images-amazon.com/images/I/41zcknWdCWL._SX300_SY300_QL70_FMwebp_.jpg"
+                      alt="Image"
+                    />
+                  </div>
+                </a>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomepageCards;
