83e23e086c9872a49f060c629455e84be6458ecb
Aseer KT <aseermiqdad@gmail.com>
2021-07-26 21:47:26 +0530

    Extracted the logic for rendering Navbar & Footer from App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import './Styles/style.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Navbar from './Components/Navbar/Navbar';
+import ShowNavFooter from './ShowNavFooter';
 import Homepage from './Components/Homepage/Homepage';
 import MyCart from './Components/CartPage/MyCart';
 import SignIn from './Components/SignIn/SignIn';
-import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Navbar />
-          <Homepage />
-        </Route>
-        <Route exact path="/register">
-          <Homepage />
-          <Navbar />
-        </Route>
-        <Route exact path="/signin">
-          <SignIn />
-        </Route>
-        <Route exact path="/cart">
-          <Navbar />
-          <MyCart />
-          <Footer />
-        </Route>
-        <Route exact path="/products">
-          <Navbar />
-          {/* Larry your product Component goes here */}
-          <Footer />
-        </Route>
-      </Switch>
+      <ShowNavFooter>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/register">
+            <Homepage />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/cart">
+            <MyCart />
+          </Route>
+          <Route exact path="/products">
+            {/* Larry your product Component goes here */}
+          </Route>
+        </Switch>
+      </ShowNavFooter>
     </Router>
   );
 }
